9d8d670e0827c8e9be378859f92a52f77b91c11f
eunjeong <eun646@nate.com>
2022-08-09 16:42:52 +0900

    gitkraken pj 생성 및 기능 추가

---
+++ src/main/java/com/example/gitkrakentest3/GitKrakenTest3Application.java
@@ -0,0 +1,13 @@
+package com.example.gitkrakentest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitKrakenTest3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitKrakenTest3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitkrakentest3/GitKrakenTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitkrakentest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitKrakenTest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
